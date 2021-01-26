477b481b9b69ae1dec7c5134cf22df0ac39408bb
Sunil Ale <22517595+sunilale0@users.noreply.github.com>
2021-01-25 19:24:39 -0500

    set up static files directory for production

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -122,4 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
